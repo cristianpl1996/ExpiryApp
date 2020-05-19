5b16347b0cd87d59615c1b2a9154cdf9a488df90
Cristian Patiño <c.patino@utp.edu.co>
2020-05-19 18:51:28 -0500

    ExpiryApp Prototipo

---
+++ Assets/CustomTrackableEventHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public bool isVideoEnabled;
+    public string url;
+    [Space]
+    public GameObject videoGO;
+
+    private Button _interactionBtn;
+    private VideoPlayer _videoPlayer;
+    private RawImage _rawImage;
+
+    private void Awake() {
+        _interactionBtn = videoGO.GetComponent<Button>();
+        _videoPlayer = videoGO.GetComponent<VideoPlayer>();
+        _rawImage = videoGO.GetComponent<RawImage>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        _interactionBtn.onClick.AddListener(OpenUrl);
+        StartCoroutine(PrepareVideo());
+    }
+
+    private IEnumerator PrepareVideo()
+    {
+        _videoPlayer.Prepare();
+
+        while (!_videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+
+        _rawImage.texture = _videoPlayer.texture;
+
+        isVideoEnabled = true;
+    }
+
+    private void OpenUrl() {
+        Application.OpenURL(url);
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if(isVideoEnabled) _videoPlayer.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if(isVideoEnabled) _videoPlayer.Pause();
+    }
+	
+	public void Play() {
+		if(isVideoEnabled) _videoPlayer.Play();
+	}
+	
+	public void Pause() {
+		if(isVideoEnabled) _videoPlayer.Pause();
+	}
+}
---
+++ Assets/SimpleCloudHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vuforia;
+using UnityEngine.Video;
+
+public class SimpleCloudHandler : MonoBehaviour, IObjectRecoEventHandler { 
+    private CloudRecoBehaviour mCloudRecoBehaviour; 
+    private bool mIsScanning = false;
+    private string mTargetMetadata = "";
+    public ImageTargetBehaviour ImageTargetTemplate_Alcohol;
+	public ImageTargetBehaviour ImageTargetTemplate_Levepromazina;
+	public ImageTargetBehaviour ImageTargetTemplate_Albendazol;
+	public ImageTargetBehaviour ImageTargetTemplate_Metoclopramida;
+// Use this for initialization 
+    void Start () { 
+// register this event handler at the cloud reco behaviour 
+        mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>(); 
+		
+        if (mCloudRecoBehaviour) 
+        { 
+            mCloudRecoBehaviour.RegisterEventHandler(this); 
+        } 
+    }
+
+    public void OnInitialized(TargetFinder targetFinder) {
+        Debug.Log ("Cloud Reco initialized");
+    }
+    public void OnInitError(TargetFinder.InitState initError) {
+        Debug.Log ("Cloud Reco init error " + initError.ToString());
+    }
+    public void OnUpdateError(TargetFinder.UpdateState updateError) {
+        Debug.Log ("Cloud Reco update error " + updateError.ToString());
+    }
+
+    public void OnStateChanged(bool scanning) {
+        mIsScanning = scanning;
+        if (scanning)
+        {
+            // clear all known trackables
+            var tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            tracker.GetTargetFinder<ImageTargetFinder>().ClearTrackables(false);
+        }
+    }
+
+    // Here we handle a cloud target recognition event
+    public void OnNewSearchResult(TargetFinder.TargetSearchResult targetSearchResult) {
+        TargetFinder.CloudRecoSearchResult cloudRecoSearchResult = 
+            (TargetFinder.CloudRecoSearchResult)targetSearchResult;
+        // do something with the target metadata
+        mTargetMetadata = cloudRecoSearchResult.MetaData;
+
+		if ((mTargetMetadata == "alcohol")) {
+			if (ImageTargetTemplate_Alcohol) { 
+				// enable the new result with the same ImageTargetBehaviour: 
+				ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+				tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate_Alcohol.gameObject);
+			}
+		} else if ((mTargetMetadata == "levepromazina")) {
+			if (ImageTargetTemplate_Levepromazina) { 
+				// enable the new result with the same ImageTargetBehaviour: 
+				ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+				tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate_Levepromazina.gameObject);
+			}
+		} else if ((mTargetMetadata == "albendazol")) {
+			if (ImageTargetTemplate_Albendazol) { 
+				// enable the new result with the same ImageTargetBehaviour: 
+				ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+				tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate_Albendazol.gameObject);
+			}
+		} else if ((mTargetMetadata == "metoclopramida")) {
+			if (ImageTargetTemplate_Metoclopramida) { 
+				// enable the new result with the same ImageTargetBehaviour: 
+				ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+				tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate_Metoclopramida.gameObject);
+			}
+		} 
+        // stop the target finder (i.e. stop scanning the cloud)
+        mCloudRecoBehaviour.CloudRecoEnabled = false;
+    }
+
+	public void Click() {
+		if (!mIsScanning) {
+            mCloudRecoBehaviour.CloudRecoEnabled = true;
+		}
+	}
+}
